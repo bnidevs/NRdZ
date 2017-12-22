47d7f17482af2defd4af2098d673aaa4125c66fe
Bill <bni@stuy.edu>
2017-12-22 16:25:26 -0500

    modifyPowerup

--- Powerup.java
+++ Powerup.java
@@ -1,6 +1,8 @@
 public abstract class Powerup{
-	String name;
-	int multiplier;
+	public final String asciiIcon = "[]";
+	public String name;
+	public int multiplier;
+	public String symbol;
 
 	public abstract void modifier();
 }

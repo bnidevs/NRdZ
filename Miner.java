98b7706f1d646e77f624cb0d487583436ea2bef2
Bill <bni@stuy.edu>
2017-12-22 16:40:57 -0500

    mineradded

---
+++ Miner.java
@@ -0,0 +1,9 @@
+public abstract class Miner{
+	public int additive;
+	public int multiplic;
+	public String name;
+
+	public Miner(String n){
+		name = n;
+	}
+}
--- Powerup.java
+++ Powerup.java
@@ -1,8 +1,14 @@
 public abstract class Powerup{
-	public final String asciiIcon = "[]";
+	public String asciiIcon = "[]";
 	public String name;
 	public int multiplier;
 	public String symbol;
 
 	public abstract void modifier();
+
+	public Powerup(String n){
+		name = n;
+		symbol = name.substring(0,1);
+		asciiIcon = asciiIcon.substring(0,1) + symbol + asciiIcon.substring(1);
+	}
 }

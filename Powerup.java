5b94b48194110aba5cc57aa55c6203d2afc8dbcb
raunakchowdhury <rchowdhury5@stuy.edu>
2017-12-22 20:11:30 -0500

    changed powerup to interface

--- Powerup.java
+++ Powerup.java
@@ -1,16 +1,17 @@
-public abstract class Powerup{
-	public String asciiIcon = "[]";
-	public String name;
-	public int multiplier;
-	public String symbol;
-	public int refreshTime;
-	public int timer;
+public interface Powerup {
 
-	public abstract void modifier();
+public String asciiIcon = "[]";
+public String name;
+public int multiplier;
+public String symbol;
+public int refreshTime;
+public int timer;
 
-	public Powerup(String n){
-		name = n;
-		symbol = name.substring(0,1);
-		asciiIcon = asciiIcon.substring(0,1) + symbol + asciiIcon.substring(1);
-	}
+public abstract void modifier();
+
+public Powerup(String n){
+								name = n;
+								symbol = name.substring(0,1);
+								asciiIcon = asciiIcon.substring(0,1) + symbol + asciiIcon.substring(1);
+}
 }

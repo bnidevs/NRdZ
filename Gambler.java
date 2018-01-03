d0b11cc0460f1e2d0fdc11e8170f317e789627bc
raunakchowdhury <rchowdhury5@stuy.edu>
2018-01-02 20:29:30 -0500

    restructured Gambler

--- Gambler.java
+++ Gambler.java
@@ -1,10 +1,17 @@
 public class Gambler extends Helper {
 
 private int _luck;   //the amount of luck the lottery is increased by
+private final int _glevel;
+
 
 public Gambler(){
         super();
         _baseGold = 1;
+        _glevel = _level;
+}
+
+public int level(){ //overwritten accessor for level
+        return _glevel;
 }
 
 }//end class Gambler

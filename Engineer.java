41cf9240575e2cd80fcffb5e5e547d2f52af629e
raunakchowdhury <rchowdhury5@stuy.edu>
2018-01-05 09:08:02 -0500

    eliminated the baseGold variable and added accessors for the other two variables

--- Engineer.java
+++ Engineer.java
@@ -1,13 +1,11 @@
 //A Helper that porduces gold upon key press
 public class Engineer extends Helper {
 
-private int _baseGoldPKP;   //base value of the gold
 private int _additionGoldPKP;   //addition bonus received from upgrades
-private int _multiGoldPKP;   //multiplier bonus received from upgrades
+private double _multiGoldPKP;   //multiplier bonus received from upgrades
 
 public Engineer(){   //set level to 1 and upgrade to 0
         super();
-        _baseGoldPKP = 1;
         _additionGoldPKP = 0;
         _multiGoldPKP = 1;
 }
@@ -15,6 +13,14 @@ public Engineer(){   //set level to 1 and upgrade to 0
 public Engineer(String name){ //overloaded consturctor that gives naming functionality
         this();
         _name = name;
+}
+
+public int additionGold(){ //accessor the addition variable
+        return _additionGoldPKP;
+}
+
+public double multiGold(){ //accessor for the multiplier variable
+        return _multiGoldPKP;
 }
 
 }//end class Engineer
--- Miner.java
+++ Miner.java
@@ -1,13 +1,11 @@
 // Helper that produces gold per second
 public class Miner extends Helper {
 
-private int _baseGoldPS;   //base value of the gold
 private int _additionGoldPS;   //addition bonus received from upgrades
-private int _multiGoldPS;   //multiplier bonus received from upgrades
+private double _multiGoldPS;   //multiplier bonus received from upgrades
 
 public Miner(){
         super();
-        _baseGoldPS = 1;
         _additionGoldPS = 0;
         _multiGoldPS = 1;
 }
@@ -15,6 +13,14 @@ public Miner(){
 public Miner(String name){ //overloaded consturctor that gives naming functionality
         this();
         _name = name;
+}
+
+public int additionGold(){ //accessor the addition variable
+        return _additionGoldPS;
+}
+
+public double multiGold(){ //accessor for the multiplier variable
+        return _multiGoldPS;
 }
 
 }//end class Miner

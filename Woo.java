4ce799121860f2c87419f011d6fd38735595699a
Bill <bni@stuy.edu>
2018-01-08 13:00:12 -0500

    editWooMovedUXtoUserInterface

--- Woo.java
+++ Woo.java
@@ -1,6 +1,7 @@
 import cs1.Keyboard;
 import terminalTxt.TerminalTxtProcessing;
 import java.util.ArrayList;
+import ui.UserInterface;
 
 public class Woo {
 	public static int _gold = 0;
@@ -16,83 +17,32 @@ public class Woo {
 
 	public static String keyPress;
 
-	public static boolean gameMainScreen;
+	public static String gameScreen;
+	public static boolean status = true;
 
 	public static void main(String[] args) {
-		System.out.println(TerminalTxtProcessing.terminalCenter("                 WELCOME TO\n /¯\\  /¯\\ |    |¯\\ |¯¯¯ | |\\  | /¯\\ |¯¯ |¯¯\\\n|  _ |   ||    |  ||--  | | \\ ||  _ |-- |--/\n \\_/  \\_/ |___ |_/ |    | |  \\| \\_/ |__ |  \\\n\n\n\n        Tap SPACE + ENTER to start"));
+		UserInterface.introUI();
 		tapMech();
 
-		ArrayList<String> testtxtarr = new ArrayList<String>();
+		UserInterface.firstGold();
 
-		String testtxt1 = "";
-		String testtxt2 = "";
-		String testtxt3 = "";
-		String testtxt4 = "";
-		String testtxt5 = "";
-		String testtxt6 = "";
+		gameScreen = "main";
 
-		testtxt1 += TerminalTxtProcessing.terminalCenter("------------------\n|                |\n|                |\n------------------\n");
-
-		testtxt2 += TerminalTxtProcessing.terminalCenter("  ------------\n /\\           \\ \n/  \\           \\ \n----------------\n");
-
-		testtxt3 += TerminalTxtProcessing.terminalCenter("  --------\n / \\     \\\n/   \\     \\\n------------\n");
-
-		testtxt4 += TerminalTxtProcessing.terminalCenter("  --\n /  \\\n/    \\\n------\n");
-
-		testtxt5 += TerminalTxtProcessing.terminalCenter("  --------\n /      / \\\n/      /   \\\n------------\n");
-
-		testtxt6 += TerminalTxtProcessing.terminalCenter("  ------------\n /           /\\\n/           /  \\\n----------------\n");
-
-		testtxtarr.add(testtxt1);
-		testtxtarr.add(testtxt2);
-		testtxtarr.add(testtxt3);
-		testtxtarr.add(testtxt4);
-		testtxtarr.add(testtxt5);
-		testtxtarr.add(testtxt6);
-
-		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 250, 1);
-
-		for(int i = 0; i < testtxtarr.size(); i++){
-			testtxtarr.set(i,testtxtarr.get(i).substring(0,10) + ("                                YOUR FIRST GOLD!\n") + testtxtarr.get(i).substring(10));
-		}
-
-		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 250, 3);
-
-		for(int i = 0; i < testtxtarr.size(); i++){
-			testtxtarr.set(i,testtxtarr.get(i).substring(0,10) + ("                    Keep pressing SPACE + ENTER to make more\n") + testtxtarr.get(i).substring(59));
-		}
-
-		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 250, 1);
-
-		for(int i = 0; i < 5; i++){
-			testtxtarr.remove(0);
-		}
-		testtxtarr.add("\n\n\n\n\n              ------\n             /     /\\\n             --------" + TerminalTxtProcessing.textRepeater("\n", 17));
-		testtxtarr.add("   \n" + _gold + " [GOLD]" + TerminalTxtProcessing.textRepeater("\n", 23));
-
-		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 250, 1);
-
-		testtxtarr.remove(0);
-		testtxtarr.remove(0);
-
-		gameMainScreen = true;
-
-		while(gameMainScreen = true){
-			if(_gold == 30){
-				testtxtarr.remove(0);
-
-				testtxtarr.add(TerminalTxtProcessing.terminalCenter("You can use gold to buy miners\n\n          ,-¯¯¯¯¯¯¯¯¯-_\n         / /\\ \\¯¯¯¯¯¯¯\n        / /  \\ \\\n       / /    \\ \\\n       ¯¯      \\ \\\n                \\ \\\n                 ¯¯\n"));
-				testtxtarr.add(TerminalTxtProcessing.terminalCenter("You can use gold to buy engineers\n\n              /| |\\\n              \\¯¯¯/\n               | |\n               | |\n               | |\n               | |\n               | |\n               | |\n              /___\\\n              \\| |/"));
-				testtxtarr.add(TerminalTxtProcessing.terminalCenter("You can also use gold to buy gamblers\n\n            /\\¯¯¯¯¯¯¯\\\n           /  \\  .    \\\n          /    \\    .  \\\n         /  .   \\       \\\n         \\      /¯¯¯----------\n          \\    /   |  .    .  |\n           \\  / .  |          |\n            \\/_____|  .    .  |\n                   |          |\n                    ----------"));
-				testtxtarr.add(TerminalTxtProcessing.terminalCenter("And powerups!\n       ____\n      |    |\n      |    |\n      |    |\n \\¯¯¯¯     |\n  \\        |\n   \\    |¯¯\n    \\   |\n     \\  |\n      \\ |\n       \\|"));
-				testtxtarr.add(TerminalTxtProcessing.terminalCenter("Press S + ENTER to see what the Shop has"));
-
-				TerminalTxtProcessing.sleeperPrinter(testtxtarr, 3000, 1);
+		while(status){
+			if(gameScreen.equals("main")){
+				if(_gold == 30){
+					UserInterface.tutorial();
+					tapMech();
+				}else if(_gold < 30){
+					UserInterface.mainUIpreTut(_gold);
+					tapMech();
+				}else{
+					UserInterface.mainUIpostTut(_gold);
+					tapMech();
+				}
+			}else if(gameScreen.equals("help")){
+				UserInterface.helpUI();
 				tapMech();
-			}else{
-				tapMech();
-				testtxtarr.set(0, testtxtarr.get(0).substring(4) + _gold + " [GOLD]" + TerminalTxtProcessing.textRepeater("\n", 22));
-				System.out.println(testtxtarr.get(0));
 			}
 		}
 	}
@@ -103,8 +53,12 @@ public class Woo {
 		if (keyPress.equals(" ")){
 			_gold += _totalGoldPKP;
 		}else if (keyPress.equals("Q")){
-			gameMainScreen = false;
+			status = false;
 			System.exit(0);
+		}else if (keyPress.equals("H")){
+			gameScreen = "help";
+		}else if (keyPress.equals("R")){
+			gameScreen = "main";
 		}
 	}
 
---
+++ ui/UserInterface.java
@@ -0,0 +1,87 @@
+package ui;
+
+import java.util.ArrayList;
+import terminalTxt.TerminalTxtProcessing;
+
+public class UserInterface {
+
+	public static void main(String[] args){}
+
+	public static void introUI(){
+		System.out.println(TerminalTxtProcessing.terminalCenter("                 WELCOME TO\n /¯\\  /¯\\ |    |¯\\ |¯¯¯ | |\\  | /¯\\ |¯¯ |¯¯\\\n|  _ |   ||    |  ||--  | | \\ ||  _ |-- |--/\n \\_/  \\_/ |___ |_/ |    | |  \\| \\_/ |__ |  \\\n\n\n\n        Tap SPACE + ENTER to start"));
+	}
+
+	public static void firstGold(){
+		ArrayList<String> testtxtarr = new ArrayList<String>();
+
+		String testtxt1 = "";
+		String testtxt2 = "";
+		String testtxt3 = "";
+		String testtxt4 = "";
+		String testtxt5 = "";
+		String testtxt6 = "";
+
+		testtxt1 += TerminalTxtProcessing.terminalCenter("------------------\n|                |\n|                |\n------------------\n");
+
+		testtxt2 += TerminalTxtProcessing.terminalCenter("  ------------\n /\\           \\ \n/  \\           \\ \n----------------\n");
+
+		testtxt3 += TerminalTxtProcessing.terminalCenter("  --------\n / \\     \\\n/   \\     \\\n------------\n");
+
+		testtxt4 += TerminalTxtProcessing.terminalCenter("  --\n /  \\\n/    \\\n------\n");
+
+		testtxt5 += TerminalTxtProcessing.terminalCenter("  --------\n /      / \\\n/      /   \\\n------------\n");
+
+		testtxt6 += TerminalTxtProcessing.terminalCenter("  ------------\n /           /\\\n/           /  \\\n----------------\n");
+
+		testtxtarr.add(testtxt1);
+		testtxtarr.add(testtxt2);
+		testtxtarr.add(testtxt3);
+		testtxtarr.add(testtxt4);
+		testtxtarr.add(testtxt5);
+		testtxtarr.add(testtxt6);
+
+		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 250, 1);
+
+		for(int i = 0; i < testtxtarr.size(); i++){
+			testtxtarr.set(i,testtxtarr.get(i).substring(0,10) + ("                                YOUR FIRST GOLD!\n") + testtxtarr.get(i).substring(10));
+		}
+
+		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 250, 3);
+
+		for(int i = 0; i < testtxtarr.size(); i++){
+			testtxtarr.set(i,testtxtarr.get(i).substring(0,10) + ("                    Keep pressing SPACE + ENTER to make more\n") + testtxtarr.get(i).substring(59));
+		}
+
+		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 250, 1);
+
+		testtxtarr.add("\n\n\n\n\n              ------\n             /     /\\\n             --------" + TerminalTxtProcessing.textRepeater("\n", 17));
+		testtxtarr.add("   \n1 [GOLD]" + TerminalTxtProcessing.textRepeater("\n", 23));
+
+		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 250, 1);
+	}
+
+	public static void mainUIpreTut(int printGold){
+		System.out.println(printGold + " [GOLD]" + TerminalTxtProcessing.textRepeater("\n", 22));
+	}
+
+	public static void mainUIpostTut(int printGold){
+		System.out.println(printGold + " [GOLD]\n\n [S]hop\n\n [H]elp\n\n [I]nventory\n\n [Q]uit" + TerminalTxtProcessing.textRepeater("\n", 14));
+	}
+
+	public static void tutorial(){
+		ArrayList<String> testtxtarr = new ArrayList<String>();
+
+		testtxtarr.add(TerminalTxtProcessing.terminalCenter("You can use gold to buy miners\n\n          ,-¯¯¯¯¯¯¯¯¯-_\n         / /\\ \\¯¯¯¯¯¯¯\n        / /  \\ \\\n       / /    \\ \\\n       ¯¯      \\ \\\n                \\ \\\n                 ¯¯\n"));
+		testtxtarr.add(TerminalTxtProcessing.terminalCenter("You can use gold to buy engineers\n\n              /| |\\\n              \\¯¯¯/\n               | |\n               | |\n               | |\n               | |\n               | |\n               | |\n              /___\\\n              \\| |/"));
+		testtxtarr.add(TerminalTxtProcessing.terminalCenter("You can also use gold to buy gamblers\n\n            /\\¯¯¯¯¯¯¯\\\n           /  \\  .    \\\n          /    \\    .  \\\n         /  .   \\       \\\n         \\      /¯¯¯----------\n          \\    /   |  .    .  |\n           \\  / .  |          |\n            \\/_____|  .    .  |\n                   |          |\n                    ----------"));
+		testtxtarr.add(TerminalTxtProcessing.terminalCenter("And powerups!\n       ____\n      |    |\n      |    |\n      |    |\n \\¯¯¯¯     |\n  \\        |\n   \\    |¯¯\n    \\   |\n     \\  |\n      \\ |\n       \\|"));
+		testtxtarr.add(TerminalTxtProcessing.terminalCenter("Press S + ENTER to see what the Shop has"));
+
+		TerminalTxtProcessing.sleeperPrinter(testtxtarr, 3000, 1);
+	}
+
+	public static void helpUI(){
+		System.out.println("[R]eturn\n\nMiners produce gold based on time.\n\nEngineers give your SPACE + ENTERs more gold.\n\nGamblers increase your chance of winning the lottery.\nYou have a 1/2000 base chance of winning the lottery every second.\n\nEarthquakes, thunderstorms and bankruptcies are also random events.\nEach has a 1/1000 chance per second of occuring.\n\nEarthquakes kill 1/4 of your miners at random.\nThunderstorms kill 1/4 of your engineers at random.\nBankruptcies kill 1/4 of your gamblers at random.\n\nA robbery has a 1/10000 chance of occuring per second.\nA robbery empties the bank of all your gold.\n\n\n\n\n\n\n");
+	}
+
+}//end class UserInterface

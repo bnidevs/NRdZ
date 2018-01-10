c1529a54110e311055d4917bcffa536b056eab43
Maggie <mzhao3@stuy.edu>
2018-01-09 22:48:21 -0500

    Changed all 2D arrays into ArrayLists. :(

--- DataStorage.java
+++ DataStorage.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import helper.*;
+//import helper.*;
 
 public class DataStorage {
     
@@ -18,8 +18,8 @@ public class DataStorage {
     
     // METHODS =============================================
 
-    public String toString(ArrayList<String[]> s) {
-	String foo = " | Name \t Level \t Upgrade Tier \t Gold |";
+    public static String toString(ArrayList<String[]> s) {
+	String foo = " | Name \t Level \t Upgrade \t Gold |\n";
 	for (int i = 0; i < s.size(); i ++ ) {
 	    foo += " | ";
 	    for (int j = 0; j < s.get(i).length; j++ ) {
@@ -42,7 +42,7 @@ public class DataStorage {
     public String gamblerList(){
 	return toString(_gamblers); 
     }
-    
+    /**
     public void expand(String[][] s) {
 	String[][] temp = new String[s.length + 1] [4];
 	for ( int row = 0; row < s.length ; row ++) {
@@ -68,8 +68,8 @@ public class DataStorage {
         String[] lastrow = {g.name(), g.level() + "", g.upgrade(), 0 + ""} ;
         _gamblers.add(lastrow);
     }
-
-    public String sortList(ArrayList<String[]> helper, int col) {
+    **/
+    public static String sortList(ArrayList<String[]> helper, int col) {
 	String[][] names = new String[helper.size()][2];
 	for (int x = 0; x < helper.size(); x ++ ) {
 	    names[x][0] = x + "";
@@ -77,9 +77,9 @@ public class DataStorage {
 	}
 	insertionSortV(names); 
 	ArrayList<String[]>temp = new ArrayList<String[]>(helper.size());
-	for (int y = 0; y < temp.size(); y++) {
+	for (int y = 0; y < helper.size(); y++) {
 	    int spot = Integer.parseInt(names[y][0]);
-	    temp.set(y,  helper.get(spot));
+	    temp.add(y, helper.get(spot));
 	}
 	helper = temp;
 	return toString(helper);
@@ -89,21 +89,48 @@ public class DataStorage {
     {
 	for( int partition = 1; partition < data.length; partition++ ) {
 	    for( int i = partition; i > 0; i-- ) {
-		if (( data[i][1]).compareTo( data[i-1][1] ) < 0 ) {
-		    String[] temp = data[i];
-		    data[i] = data[i-1];
-		    data[i-1] = temp;
+		if (Character.isDigit(data[i][1].charAt(0))){
+                    int first = Integer.parseInt(data[i-1][1]);
+                    int second = Integer.parseInt(data[i][1]);
+		    if ( second < first) {
+			String[] temp = data[i];
+			data[i] = data[i-1];
+			data[i-1] = temp;
+		    }
+		    
+		    else
+			break;
 		}
 		
-		else
-		    break;
+                else {
+                    String first = data[i-1][1];
+                    String second = data[i][1];
+		    if (( second).compareTo( first ) < 0 ) {
+			String[] temp = data[i];
+			data[i] = data[i-1];
+			data[i-1] = temp;
+		    }
+
+		    else
+			break;
+		}
+		
             }
 	}
     }//end insertionSortV
 
     public static void main(String[] args) {
+	ArrayList<String[]> _miners = new ArrayList<String[]>();
+	_miners.add(new String[]{"Robert" , "1", "3", "6"});
+	_miners.add(new String[]{"Joseph", "34", "22", "35"});
+	_miners.add(new String[]{"Broseph", "12", "34", "88"});
+	_miners.add(new String[]{"Cindy", "2", "5", "6"});
 	
-
+	System.out.println(toString(_miners));
+	System.out.println(sortList(_miners, 0));
+	System.out.println(sortList(_miners, 1));
+	System.out.println(sortList(_miners, 2));
+	System.out.println(sortList(_miners, 3));
 
     }
     
